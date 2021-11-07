d30c171b9b8e9e51620e50a8d90c96bb21cc41b0
Mamelukki <sara.m.salmi@gmail.com>
2021-11-07 14:32:30 +0200

    Fix?

--- src/main/java/projekti/PostgreSQLDialectCustom.java
+++ src/main/java/projekti/PostgreSQLDialectCustom.java
@@ -5,11 +5,7 @@ import org.hibernate.dialect.PostgreSQL82Dialect;
 import org.hibernate.type.descriptor.sql.BinaryTypeDescriptor;
 import org.hibernate.type.descriptor.sql.SqlTypeDescriptor;
 
-public class CustomPostgreSQLDialect extends PostgreSQL82Dialect {
-
-    public CustomPostgreSQLDialect() {
-        registerColumnType(Types.BLOB, "bytea");
-    }
+public class PostgreSQLDialectCustom extends PostgreSQL82Dialect {
 
     @Override
     public SqlTypeDescriptor remapSqlTypeDescriptor(SqlTypeDescriptor sqlTypeDescriptor) {
