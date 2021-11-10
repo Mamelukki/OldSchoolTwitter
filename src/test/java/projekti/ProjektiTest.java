1b42212e9e245fa3de9e33cf42f111c702aa26ab
Mamelukki <sara.m.salmi@gmail.com>
2021-11-10 17:41:33 +0200

    Fixed one condition

--- src/test/java/projekti/ProjektiTest.java
+++ src/test/java/projekti/ProjektiTest.java
@@ -4,13 +4,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProjektiTest {
 
 
+    @Autowired
+    private MockMvc mockMvc;
+    
     @Autowired
     private AccountRepository accountRepository;
 
